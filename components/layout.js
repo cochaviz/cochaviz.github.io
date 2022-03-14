a179a2fe3bb92ab640bc9eabc67ec8248e21909d
Zohar Cochavi <cochavi.zohar@protonmail.com>
2022-03-14 20:58:21 +0100

    First commit

---
+++ components/layout.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Layout({children}) {
+  return (
+    <div className="dark:bg-neutral-900 flex flex-col min-h-screen">
+      <header className="bg-black sm:mb-8 py-4 px-8 ">
+        <div className="container max-w-4xl mx-auto flex justify-between">
+          <Link href="/">
+            <a className="text-white no-underline my-auto hover:text-green-600">~/</a>
+          </Link>
+
+          <span className="hidden sm:block mx-auto my-auto text-yellow-600">cochaviz@bunkernet.dev</span>{' '}
+          <span className="block sm:hidden mx-auto my-auto text-yellow-600">cochaviz</span>{' '}
+
+          <div className="flex gap-x-2">
+            <a href="https://www.github.com/zoharcochavi"> github </a>
+            <a href="https://www.linkedin.com/in/cochaviz"> linkedin </a>
+          </div>
+        </div>
+      </header>
+
+      <main className="container max-w-4xl mx-auto flex-1 sm:px-8">{children}</main>
+
+      <footer className="dark:bg-black mt-8 py-4">
+        <div className="container mx-auto text-neutral-500 flex justify-center">
+        &copy; 2022 - Zohar Cochavi
+        </div>
+      </footer>
+    </div>
+  );
+}
---
+++ pages/post/[slug].js
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import markdownIt from 'markdown-it';
+import highlightjs from 'markdown-it-highlightjs';
+import Head from 'next/head';
+
+const md = markdownIt().use(highlightjs);
+
+export async function getStaticPaths() {
+    const files = fs.readdirSync('posts');
+
+    const paths = files.map((fileName) => ({
+        params: {
+            slug: fileName.replace('.md', ''),
+        },
+    }));
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({params: {slug}}) {
+  const fileName = fs.readFileSync(`posts/${slug}.md`, 'utf-8');
+  const {data: frontmatter, content} = matter(fileName);
+  return {
+    props: {
+      frontmatter,
+      content,
+    },
+  };
+}
+
+export default function PostPage({frontmatter, content}) {
+  return (
+    <div className="prose dark:prose-invert mx-auto px-4 pt-4 sm:pt-0">
+      <Head>
+        <title>{frontmatter.title}</title>
+        <meta property="og:title" content={frontmatter.metaTitle} key="title" />
+        <meta property="og:description" content={frontmatter.metaDesc} key="description" />
+      </Head>
+      <h1>{frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{__html: md.render(content)}} />
+    </div>
+  );
+}
