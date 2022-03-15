605b03f4a5f8cc2e6ab727c199f107fe4657d4d5
Zohar Cochavi <cochavi.zohar@protonmail.com>
2022-03-15 17:52:19 +0100

    Add custom favicon

--- pages/_document.js
+++ pages/_document.js
@@ -10,16 +10,21 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* google fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Fira+Mono&display=swap"
-            rel="stylesheet"
-          />
+          <link href="https://fonts.googleapis.com/css2?family=Fira+Mono&display=swap" rel="stylesheet" />
+
+          {/* favicon */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
           <NextScript />
+
           {/* clicky traffic analysis */}
           <script async src="//static.getclicky.com/101354370.js"></script>
           <noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/101354370ns.gif" /></p></noscript>
