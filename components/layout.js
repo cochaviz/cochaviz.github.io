c9e0a49b6dc3d5dedea016191df64222923a3009
Zohar Cochavi <cochavi.zohar@protonmail.com>
2022-03-15 12:08:50 +0100

    Fix light mode not working properly

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,16 @@
 import Link from 'next/link';
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
     <div className="dark:bg-neutral-900 flex flex-col min-h-screen">
-      <header className="bg-black sm:mb-8 py-4 px-8 ">
+      <header className="bg-neutral-200 dark:bg-black sm:mb-8 py-4 px-8 ">
         <div className="container max-w-4xl mx-auto flex justify-between">
           <Link href="/">
-            <a className="text-white no-underline my-auto hover:text-green-600">~/</a>
+            <a className="no-underline my-auto">~/</a>
           </Link>
 
-          <span className="hidden sm:block mx-auto my-auto text-yellow-600">cochaviz@bunkernet.dev</span>{' '}
-          <span className="block sm:hidden mx-auto my-auto text-yellow-600">cochaviz</span>{' '}
+          <span className="hidden sm:block mx-auto my-auto text-green-600 dark:text-yellow-600">cochaviz@bunkernet.dev</span>{' '}
+          <span className="block sm:hidden mx-auto my-auto text-green-600 dark:text-yellow-600">cochaviz</span>{' '}
 
           <div className="flex gap-x-2">
             <a href="https://www.github.com/zoharcochavi"> github </a>
@@ -21,9 +21,9 @@ export default function Layout({children}) {
 
       <main className="container max-w-4xl mx-auto flex-1 sm:px-8">{children}</main>
 
-      <footer className="dark:bg-black mt-8 py-4">
+      <footer className="bg-neutral-200 dark:bg-black mt-8 py-4">
         <div className="container mx-auto text-neutral-500 flex justify-center">
-        &copy; 2022 - Zohar Cochavi
+          &copy; 2022 - Zohar Cochavi
         </div>
       </footer>
     </div>
