99e20fd0ebedee9d8977184359707edd29ea91de
Zohar Cochavi <cochavi.zohar@protonmail.com>
2022-03-15 18:02:14 +0100

    Don't forget to credit!

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Fira+Mono&display=swap" rel="stylesheet" />
 
-          {/* favicon */}
+          {/* favicon - Copyright 2020 Twitter, Inc and other contributors */}
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
